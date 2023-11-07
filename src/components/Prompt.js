0a6ac299429eb493f441e19787cfb7b031bd43b2
lbCyber <49038315+lbCyber@users.noreply.github.com>
2023-11-07 11:25:54 -0500

    small functionality refinements

--- src/components/Prompt.js
+++ src/components/Prompt.js
@@ -8,20 +8,22 @@ const { OpenAI } = require("openai");
 const aiConfig = {
   organization: process.env.REACT_APP_OPENAI_ORG,
   apiKey: process.env.REACT_APP_OPENAI_API_KEY,
-  dangerouslyAllowBrowser: true,
+  dangerouslyAllowBrowser: true, //not actually in danger, info is stored in react env
 };
 
 const openaiStart = new OpenAI(aiConfig);
 
 const Prompt = () => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [promptResult, setPromptResult] = useState("");
   const [promptCount, setPromptCount] = useState(0);
   const [submitDisabled, setSubmitDisabled] = useState(false);
   const [typeReady, setTypeReady] = useState(false);
   const [submissions, setSubmissions] = useState([]);
-  const [genre, setGenre] = useState("")
-  const [protag, setProtag] = useState("")
+  const [genre, setGenre] = useState("");
+  const [genres, setGenres] = useState([]);
+  const [protag, setProtag] = useState("");
+  const [protags, setProtags] = useState([]);
 
   const fetchPrompt = (e) => {
     e.preventDefault();
@@ -40,7 +42,7 @@ const Prompt = () => {
         max_tokens: tokenLength,
         temperature: randomness,
       }).then(async (res, rej) => {
-        const resPrompt = await res.choices[0].text.replace(/"\n/g, "")
+        const resPrompt = await res.choices[0].text.replace(/\n/g, "").replace('"',"")
         if (typeof resPrompt === "string") {
           setPromptCount(promptCount + 1);
           setPromptResult(resPrompt);
@@ -52,15 +54,19 @@ const Prompt = () => {
       }).finally(()=>{
         setLoading(false);
         setTypeReady(true)
+
         doTimer();
       });
     }
-
+    setGenres([...genres, genre])
+    setProtags([...protags, protag])
     const prompt = `write a good single line idea for a ${genre} story. The protagonist is ${protag}.`
     const tokenLength = 100;
     const randomness = 0;
     setLoading(true)
     doPrompt();
+    setGenre("")
+    setProtag("")
   };
 
   return (
@@ -82,7 +88,7 @@ const Prompt = () => {
           }}
           onInit={(typewriter) => {
             typewriter.typeString(promptResult).callFunction(() => {
-              setSubmissions([...promptResult])
+              setSubmissions([...submissions, promptResult])
             }).start();
           }}
         /> : null
